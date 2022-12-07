9ec60b68a12f982cff7c703ae4c49e6460c16d82
akeyless <github@akeyless.io>
2022-12-07 13:52:55 +0000

    Release v3.0.1

--- akeyless/__init__.py
+++ akeyless/__init__.py
@@ -15,7 +15,7 @@
 
 from __future__ import absolute_import
 
-__version__ = "3.0.0"
+__version__ = "3.0.1"
 
 # import apis into sdk package
 from akeyless.api.v2_api import V2Api
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "akeyless"
-VERSION = "3.0.0"
+VERSION = "3.0.1"
 # To install the library, run the following
 #
 # python setup.py install
