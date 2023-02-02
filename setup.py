cd81e5ef9e30443792bd8fa5154dbb9890c81aed
akeyless <github@akeyless.io>
2023-02-02 12:51:09 +0000

    Release v3.2.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "akeyless"
-VERSION = "3.2.2-c"
+VERSION = "3.2.3"
 # To install the library, run the following
 #
 # python setup.py install
